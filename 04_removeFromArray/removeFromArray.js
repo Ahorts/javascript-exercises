0cbc0d5dc964c8be8df83744bf11af554321832f
Ahorts <godswayahorts@gmail.com>
2024-10-01 01:10:36 +0000

    fix: decent solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,8 @@
-const removeFromArray = function (array, value1 = null, value2 = null, value3 = null, value4 = null) {
+const removeFromArray = function (array, ...values) {
     
    let i = 0;
    while (i < array.length){
-        if (array[i] === value1 || array[i] === value2 || array[i] === value3 || array[i] === value4) {
+        if (values.includes(array[i])) {
             array.splice(i, 1);   
         } else {
             i++;
