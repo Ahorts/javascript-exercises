e7b5920eabfb78e136cc36a9a70201ead1d2acb8
Ahorts <godswayahorts@gmail.com>
2024-10-01 00:24:18 +0000

    Write garbage solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, value1 = null, value2 = null, value3 = null, value4 = null) {
+    
+   let i = 0;
+   while (i < array.length){
+        if (array[i] === value1 || array[i] === value2 || array[i] === value3 || array[i] === value4) {
+            array.splice(i, 1);   
+        } else {
+            i++;
+        }
+    };
+
+
+    return array;
 };
 
 // Do not edit below this line
