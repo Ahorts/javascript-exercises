1969b168287c1eebab0c0803f23ce414c9ef2686
Ahorts <godswayahorts@gmail.com>
2024-10-01 01:55:27 +0000

    solve exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (value1, value2) {
+    let sum = 0;
+    if (value1 < 0 || value2 < 0 || !(Number.isInteger(value1) && Number.isInteger(value2))) {
+        return "ERROR"
+    };
 
+    if (value1 < value2) {
+        for (let i = value1; i <= value2; i++) {
+            sum += i;
+        };
+    } else {
+        for (let i = value2; i <= value1; i++) {
+            sum += i;
+        };
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
